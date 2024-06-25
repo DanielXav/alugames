d5847f5f410de7dc60d7a683a353f7aff0bb4ac4
Daniel Xavier <dxavier194@gmail.com>
2024-06-25 15:10:28 -0300

    classes e tipos de atributos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,10 +10,15 @@ fun main() {
     val request = HttpRequest.newBuilder()
         .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
         .build()
-    
+
     val response = client
         .send(request, BodyHandlers.ofString())
 
     val json = response.body();
     println(json)
+
+    val meuJogo = Jogo()
+    meuJogo.titulo = "Batman: Arkham Asylum Game of the Year Edition"
+    meuJogo.capa = "https:\\/\\/shared.akamai.steamstatic.com\\/store_item_assets\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1702934705"
+    println(meuJogo.toString())
 }
