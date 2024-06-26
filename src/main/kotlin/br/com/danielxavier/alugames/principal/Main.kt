b9910389c0f100d3ab4df5d1aa7387d87b937239
Daniel Xavier <dxavier194@gmail.com>
2024-06-26 12:00:54 -0300

    companion object, operações com listas e extension functions

--- src/main/kotlin/br/com/danielxavier/alugames/principal/Main.kt
+++ src/main/kotlin/br/com/danielxavier/alugames/principal/Main.kt
@@ -1,44 +1,87 @@
 package br.com.danielxavier.alugames.principal
 
+import br.com.danielxavier.alugames.modelo.Gamer
 import br.com.danielxavier.alugames.modelo.Jogo
 import br.com.danielxavier.alugames.servicos.ConsumoApi
+import transformarEmIdade
 import java.util.*
 
 fun main() {
 
     val leitura = Scanner(System.`in`)
-    println("Digite um código de jogo para buscar:")
-    val busca = leitura.nextLine()
+    val gamer = Gamer.criarGamer(leitura)
+    println("Cadastro concluido com sucesso. Dados do gamer:")
+    println(gamer)
+    println("Idade do gamer: " + gamer.dataNascimento?.transformarEmIdade())
 
-    var meuJogo: Jogo? = null
+    do {
+        println("Digite um código de jogo para buscar:")
+        val busca = leitura.nextLine()
 
-    val resultado = runCatching {
-        val buscaApi = ConsumoApi()
-        val informacaoJogo = buscaApi.buscaJogo(busca)
+        var meuJogo: Jogo? = null
 
-        meuJogo = Jogo(
-            informacaoJogo.info.title,
-            informacaoJogo.info.thumb
-        )
-    }
+        val resultado = runCatching {
+            val buscaApi = ConsumoApi()
+            val informacaoJogo = buscaApi.buscaJogo(busca)
 
-    resultado.onFailure {
-        println("Jogo inexistente. Tente outro id.")
-    }
-
-    resultado.onSuccess {
-        println("Deseja inserir uma descrição personalizada? S/N")
-        val opcao = leitura.nextLine()
-        if (opcao.equals("s", true)){
-            println("Insira a descrição persinalizada para o jogo:")
-            val descricaoPersonalizada = leitura.nextLine()
-            meuJogo?.descricao = descricaoPersonalizada
-        } else {
-            meuJogo?.descricao = meuJogo?.titulo
+            meuJogo = Jogo(
+                informacaoJogo.info.title,
+                informacaoJogo.info.thumb
+            )
         }
-        println(meuJogo)
+
+        resultado.onFailure {
+            println("Jogo inexistente. Tente outro id.")
+        }
+
+        resultado.onSuccess {
+            println("Deseja inserir uma descrição personalizada? S/N")
+            val opcao = leitura.nextLine()
+            if (opcao.equals("s", true)){
+                println("Insira a descrição persinalizada para o jogo:")
+                val descricaoPersonalizada = leitura.nextLine()
+                meuJogo?.descricao = descricaoPersonalizada
+            } else {
+                meuJogo?.descricao = meuJogo?.titulo
+            }
+
+            gamer.jogosBuscados.add(meuJogo)
+        }
+
+        println("Deseha buscar um novo jogo? S/N")
+        val resposta = leitura.nextLine()
+
     }
-    resultado.onSuccess {
-        println("Busca finalizada com sucesso.")
+    while (resposta.equals("s", true))
+
+    println("Jogos buscados:")
+    println(gamer.jogosBuscados)
+
+    println("Jogos ordenados por título: ")
+    gamer.jogosBuscados.sortBy {
+        it?.titulo
     }
+    gamer.jogosBuscados.forEach {
+        println("Título: " + it?.titulo)
+    }
+
+    val jogosFiltrados = gamer.jogosBuscados.filter {
+        it?.titulo?.contains("batman", true) ?: false
+    }
+    println("\nJogos filtrados:")
+    println(jogosFiltrados)
+
+    println("Deseja excluir algum jogo da lista original? S/N")
+    val opcao = leitura.nextLine()
+    if (opcao.equals("s", true)){
+        println(gamer.jogosBuscados)
+        println("\nDigite o título do jogo que deseja excluir:")
+        val posicao = leitura.nextInt()
+        gamer.jogosBuscados.removeAt(posicao)
+    }
+
+    println("Jogos após exclusão:")
+    println(gamer.jogosBuscados)
+
+    println("Busca finalizada com sucesso.")
 }
